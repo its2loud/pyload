84b6a265759fde0fd7292696e639ecccbaaa4b71
Christoph Friedrich <christophfriedrich@imac.fritz.box>
2016-05-11 19:31:23 +0200

    cnl fixes

--- module/web/cnl_app.py
+++ module/web/cnl_app.py
@@ -18,7 +18,8 @@ except:
 def local_check(function):
     def _view(*args, **kwargs):
         if request.environ.get('REMOTE_ADDR', "0") in ('127.0.0.1', 'localhost') \
-        or request.environ.get('HTTP_HOST','0') == '127.0.0.1:9666':
+        or request.environ.get('HTTP_HOST','0') == '127.0.0.1:9666' \
+        or request.environ.get('HTTP_HOST','0') == 'localhost:9666':
             return function(*args, **kwargs)
         else:
             return HTTPError(403, "Forbidden")
@@ -74,7 +75,7 @@ def addcrypted2():
     jk = request.forms["jk"]
 
     crypted = standard_b64decode(unquote(crypted.replace(" ", "+")))
-    if JS:
+    if JS and len(JS.eval(jk+" f()"))>0: #ossp-js work around
         jk = "%s f()" % jk
         jk = JS.eval(jk)
 
